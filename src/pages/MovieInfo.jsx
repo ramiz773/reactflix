32cb5b755f5f5448a11e92797e31e36350e6df55
ramees <aleemrameestpn@gmail.com>
2023-10-27 11:58:59 +0530

    complete movie details page

--- src/pages/MovieInfo.jsx
+++ src/pages/MovieInfo.jsx
@@ -1,11 +1,67 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useFetch } from "../Hooks/useFetch";
+import Wrapper from "../Components/Wrapper";
 
 function MovieInfo() {
   let { id } = useParams();
-  console.log("id    " + id);
 
-  return <div>MovieInfo</div>;
+  const [data] = useFetch(`movie/${id}`);
+  console.log(data);
+
+  const backdrop = {
+    backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${data?.backdrop_path})`,
+  };
+
+  return (
+    <div className="moviePage" style={backdrop}>
+      <Wrapper>
+        <div className="movieContent">
+          <div className="moviePoster">
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+              alt=""
+            />
+          </div>
+          <div className="movieInfo">
+            <div className="movieTitle">
+              <h1>{data?.title}</h1>
+              <h3>{data?.tagline}</h3>
+            </div>
+            <div className="moviePlot">{data?.overview}</div>
+            <div className="movieDetails">
+              <div className="movieDetailsItem">
+                <h3>languages</h3>
+                <div>
+                  {data?.spoken_languages?.map((lang) => {
+                    return (
+                      <span key={lang.iso_639_1}>{lang.english_name}</span>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="movieDetailsItem">
+                <h3>genre</h3>
+                <div>
+                  {data?.genres?.map((genre) => {
+                    return <span key={genre.id}>{genre.name}</span>;
+                  })}
+                </div>
+              </div>
+              <div className="movieDetailsItem">
+                <h3>Runtime</h3>
+                <div>{`${data.runtime} minutes`}</div>
+              </div>
+              <div className="movieDetailsItem">
+                <h3>Rating</h3>
+                <div>{data.vote_average}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Wrapper>
+    </div>
+  );
 }
 
 export default MovieInfo;
